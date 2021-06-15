1bc3dba4aafc0cd8cce7f5f605a553fba6d7ca71
liliang <ll917274996@live.com>
2021-06-15 08:28:29 +0800

    删除不需要的代码

--- src/components/snake/main.js
+++ src/components/snake/main.js
@@ -1,13 +1,4 @@
 class Board {
-  canEat() {
-    let { arrow, food } = this
-    let { row, col } = this.snake[0]
-    return (arrow === 1 && col === food.col && row === food.row + 1)
-      || (arrow === 2 && col === food.col - 1 && row === food.row)
-      || (arrow === 3 && col === food.col && row === food.row - 1)
-      || (arrow === 4 && col === food.col + 1 && row === food.row)
-  }
-
   isEnd() {
     let { arrow, rows, cols } = this, { row, col } = this.snake[0]
     return (arrow === 1 && row === 0)
